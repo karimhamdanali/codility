c500186ac552f3ec4ff7a483456c95c8f213df1e
Karim Ali <karim.ali@cased.de>
2016-01-06 13:52:07 +0100

    Lesson 2: multiple solutions

--- src/codility/OddOccurrencesInArray.java
+++ src/codility/OddOccurrencesInArray.java
@@ -1,32 +1,52 @@
 package codility;
 
+import java.util.HashSet;
+
 public class OddOccurrencesInArray {
+	// A is non-empty
+	// N is odd
+	// It contains pairs, need to return value of unpaired element
 
 	public static void main(String[] args) {
-	System.out.println(solution(new int [] { 9, 3, 9, 3, 9, 7, 9} ));	
+		System.out.println(solution(new int[] { 9, 3, 9, 3, 9, 7, 9 }));
 
 	}
-	
+
+	// Time: O(N)
+	// Space: O(N)
 	public static int solution(int[] A) {
-        // write your code in Java SE 8
-        
-        // A is non-empty
-        // N is odd
-        // It contains pairs, need to return value of unpaired element
-        for(int i = 0; i < A.length; i++) {    
-            for(int j = 0; j < A.length; j++) {
-                if(A[i] == A[j] && i != j) {
-                    A[i] = -1;
-                    A[j] = -1;
-                    j = A.length;
-                }
-            }
-        }
-        
-        for(int i = 0; i < A.length; i++) {
-            if(A[i] != -1) return A[i];
-        }
-        
-        return 0; // should never happen
-    }
+		// write your code in Java SE 8
+		HashSet<Integer> dups = new HashSet<Integer>();
+		int elem = -1;
+
+		for (int i = 0; i < A.length; i++) {
+			if (dups.add(A[i])) {
+				elem = A[i];
+			}
+		}
+		return elem;
+	}
+
+	// Time: O(N^2)
+	// Space: O(1)
+	public static int solution3(int[] A) {
+		// write your code in Java SE 8
+		for (int i = 0; i < A.length; i++) {
+
+			for (int j = 0; j < A.length; j++) {
+				if (A[i] == A[j] && i != j) {
+					A[i] = -1;
+					A[j] = -1;
+					j = A.length;
+				}
+			}
+		}
+
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] != -1)
+				return A[i];
+		}
+
+		return 0; // should never happen
+	}
 }
