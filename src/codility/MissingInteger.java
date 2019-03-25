ccce4618458a329fab2b700344e75315e178ae54
Anton Rudacov <33105393+antonrud@users.noreply.github.com>
2019-03-25 11:33:29 +0100

    Return value in case of array containing all integers
    I believe if we get an array full of all possible integers the return of 'min' (which is 1) is not quite correct.
    I decided to return Integer.MAX_VALUE in this case.
    And the 'min' variable is not mutated during runtime, so why declare it anyway.

--- src/codility/MissingInteger.java
+++ src/codility/MissingInteger.java
@@ -25,7 +25,6 @@ public class MissingInteger {
 	public static int solution(int[] A) {
 		// write your code in Java SE 8
 		HashSet<Integer> seen = new HashSet<Integer>();
-		int min = 1;
 
 		for (int i = 0; i < A.length; i++) {
 			if (A[i] > 0) seen.add(A[i]);
@@ -35,6 +34,6 @@ public class MissingInteger {
 			if(!seen.contains(i)) return i;
 		}
 
-		return min;
+		return Integer.MAX_VALUE;
 	}
 }
